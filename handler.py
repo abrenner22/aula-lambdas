c1595e93cad5b5f231dc19d3c4a3dd86b6038daf
pduarteo <pedroduarteolv@gmail.com>
2025-09-19 18:00:00 -0300

    fix path

--- handler.py
+++ handler.py
@@ -1,7 +1,5 @@
-import json
-
 def hello_world(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps({'message': 'Hello, World!'})
+        'body': 'Hello, World!'
     }
